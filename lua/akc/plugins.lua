6624f026c4fc2d4d2b24288fc804f2dbbfd5a043
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-11-03 04:55:47 +0530

    ADD: nvim-tree

--- lua/akc/plugins.lua
+++ lua/akc/plugins.lua
@@ -1,6 +1,10 @@
 local plugins = {
-	{'andweeb/presence.nvim'},
-	{'github/copilot.vim', lazy=false}
+    {'andweeb/presence.nvim'},
+    {'github/copilot.vim', lazy=false},
+    {'nvim-tree/nvim-tree.lua',
+        dependencies = {'kyazdani42/nvim-web-devicons'},
+    },
+
 }
 
 return plugins

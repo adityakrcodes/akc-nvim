3c806d036561fe6ccdca1a91028a57b2ff5a5e2a
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-11-03 07:41:57 +0530

    ADD: lazygit keybindfs

--- lua/akc/core/remap.lua
+++ lua/akc/core/remap.lua
@@ -3,3 +3,7 @@ local map = vim.keymap
 
 -- NvimTree Keymaps
 map.set('n', '<leader>e', '<cmd>NvimTreeToggle <cr>')
+
+-- LazyGit Keymaps
+map.set('n', '<leader>lg', '<cmd>LazyGit <cr>')
+map.set('n', '<leader>lc', '<cmd>LazyGitCurrentFile <cr>')

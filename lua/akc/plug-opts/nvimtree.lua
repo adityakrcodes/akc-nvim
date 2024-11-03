6994300ba62c77f312f2239042985486e42589db
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-11-03 06:01:02 +0530

    UPDATE: config for nvim-tree

--- lua/akc/plug-opts/nvimtree.lua
+++ lua/akc/plug-opts/nvimtree.lua
@@ -1,4 +1,14 @@
 require("nvim-tree").setup{
     disable_netrw       = true,
     hijack_netrw        = true,
+    renderer = {
+        icons = {
+            web_devicons = {
+                file = {
+                    enable = true,
+                    color = true,
+                },
+            }
+        }
+    },
 }

d30b2481daaae5465ee38f46d01f046575ff724c
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-11-03 07:42:34 +0530

    UPDATE: nvim-tree command from toggle to focus

--- lua/akc/core/remap.lua
+++ lua/akc/core/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 local map = vim.keymap
 
 -- NvimTree Keymaps
-map.set('n', '<leader>e', '<cmd>NvimTreeToggle <cr>')
+map.set('n', '<leader>e', '<cmd>NvimTreeFocus <cr>')
 
 -- LazyGit Keymaps
 map.set('n', '<leader>lg', '<cmd>LazyGit <cr>')
